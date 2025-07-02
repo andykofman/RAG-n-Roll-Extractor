b90b74605ef3031a43f83a49652870f18ae999f8
A.Ali <ali.a@aucegypt.edu>
2025-07-02 13:30:37 +0300

    testing read the docs

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+project = 'RAG-n-Roll-Extractor'
+copyright = '2024, A.Ali'
+author = 'A.Ali'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+autodoc_typehints = 'description'
+autodoc_member_order = 'bysource' 
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+Welcome to RAG-n-Roll-Extractor's documentation!
+==========================================
+
+.. image:: ../assets/RAG.png
+   :alt: RAG Architecture
+   :align: center
+
+*Image source:* `LangChain Cookbook - Semi-structured and Multi-modal RAG <https://github.com/langchain-ai/langchain/blob/master/cookbook/Semi_structured_and_multi_modal_RAG.ipynb>`_
+
+RAG-n-Roll-Extractor is a professional Retrieval-Augmented Generation (RAG) extraction tool that helps process and prepare documents for RAG applications.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   installation
+   usage
+   api
+   contributing
+   changelog
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search` 
